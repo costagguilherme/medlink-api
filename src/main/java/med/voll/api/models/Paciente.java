0a41541382089c87e61ab30d2d8a6373ebb7997f
reynanwq <reynanwq@gmail.com>
2025-01-16 03:16:59 -0300

    feat: adicionando mais informacoes no Paciente e novas especialidades no enum Medico

--- src/main/java/med/voll/api/models/Paciente.java
+++ src/main/java/med/voll/api/models/Paciente.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.dtos.pacientes.PacienteDto;
 import med.voll.api.dtos.pacientes.PacientePutDto;
+import med.voll.api.enums.GeneroEnum;
+
+import java.util.Date;
 
 
 @Table(name = "pacientes")
@@ -20,15 +23,31 @@ public class Paciente {
     private Long id;
     private String nome;
     private String cpf;
+    private String telefone;
+    @Column(name = "data_nascimento")
+    private Date dataNascimento;
+
+    @Enumerated(EnumType.STRING)
+    private GeneroEnum genero;
+
+    private String email;
 
     public Paciente(PacienteDto data) {
         this.nome = data.nome();
         this.cpf = data.cpf();
+        this.telefone = data.telefone();
+        this.dataNascimento = data.dataNascimento();
+        this.genero = data.genero();
+        this.email = data.email();
     }
 
     public void update(PacientePutDto data) {
         this.nome = data.nome() != null ? data.nome() : this.nome;
         this.cpf = data.cpf() != null ? data.cpf() : this.cpf;
+        this.telefone = data.telefone() != null ? data.telefone() : this.telefone;
+        this.dataNascimento = data.dataNascimento() != null ? data.dataNascimento() : this.dataNascimento;
+        this.genero = data.genero() != null ? data.genero() : this.genero;
+        this.email = data.email() != null ? data.email() : this.email;
     }
 
   
