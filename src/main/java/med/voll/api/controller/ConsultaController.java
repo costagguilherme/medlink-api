2968dd057c6f4851282f6c0cf9d7244af516ccf4
Guilherme Costa <costa.gguilherme02@gmail.com>
2025-01-24 22:59:18 -0300

    ajustando getAllConsultas

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,13 +1,17 @@
 package med.voll.api.controller;
 
-import med.voll.api.dtos.consultas.ConsultaDto;
-import med.voll.api.models.interfaces.IConsultaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.dtos.consultas.ConsultaDto;
+import med.voll.api.models.interfaces.IConsultaRepository;
 import med.voll.api.usecases.ScheduleAppointmentUseCase;
 
 
@@ -31,6 +35,16 @@ public class ConsultaController {
     @GetMapping
     public ResponseEntity getAllConsultas() {
         var consultas = consultaRepository.findAll();
-        return ResponseEntity.ok(consultas);
+
+        var consultasCompletas = consultas.stream()
+            .map(consulta -> new Object() {
+                public final Long id = consulta.getId();
+                public final String data = consulta.getData().toString();
+                public final String medico = consulta.getMedico().getNome();
+                public final String paciente = consulta.getPaciente().getNome();
+            })
+            .toList();
+    
+        return ResponseEntity.ok(consultasCompletas);
     }
 }
--- src/main/java/med/voll/api/models/Consulta.java
+++ src/main/java/med/voll/api/models/Consulta.java
@@ -1,19 +1,21 @@
 package med.voll.api.models;
 
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import jakarta.persistence.GeneratedValue;
-
-import jakarta.persistence.ManyToOne;
-
-import jakarta.persistence.JoinColumn;
-
-import java.time.LocalDateTime;
 
 @Table(name = "consultas")
 @Entity(name = "Consulta")
